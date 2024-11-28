d8c45a06a4c1c50911cff9c365f8e5f705c27614
Tharindu-wolfsl <tpriyashan1996@gmail.com>
2024-11-28 20:40:25 +0530

    env / database configuration added

---
+++ app/config/database.js
@@ -0,0 +1,6 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_USERNAME,
+});
